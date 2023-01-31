4d69828cb36744746bcc9f641dd4cde2c60f08ae
Ali Marawan <103090890+Alii2121@users.noreply.github.com>
2023-01-31 18:28:35 +0200

    Delete IaC_Terraform directory

--- IaC_Terraform/EC2/private-ec2.sh
+++
@@ -1,9 +0,0 @@
-#!usr/bin/bash
-sudo chmod +x private-ec2.sh
-sudo apt update -y
-sudo apt install nginx -y
-sudo systemctl start nginx 
-IP=$(hostname -I)
-sudo cd var/www/html
-sudo echo "Hello from Private instance ${IP}" > index.html 
-sudo systemctl restart nginx
